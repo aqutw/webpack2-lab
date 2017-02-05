7142026e87d84eb66a4c54245da0e5d1fee52878
aqutw <aquarian.ex+github@gmail.com>
2017-02-05 17:10:30 +0800

    re-build for image src small/big

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,10 @@
+import big from '../assets/big.jpeg';
+import small from '../assets/small.jpeg';
 import '../styles/image_viewer.css';
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400'
+const r = Math.random();
+image.src = r>0.5 ? ('build/' + big) : small;
+console.log(r, big, small);
 
 document.body.appendChild(image);
